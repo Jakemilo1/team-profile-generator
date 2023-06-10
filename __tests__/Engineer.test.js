ec09b113294f1af278c72e7a1d024b95355286b4
Jacob Milojkovich <jacobmilojkovich@jacobs-mbp.mynetworksettings.com>
2023-06-10 17:53:44 -0400

    added comments

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -8,12 +8,15 @@ test('checks if all info for Engineer is provided and accurate', () => {
     const role = "Engineer";
 
     const engineer = new Engineer(name, id, email, github);
+    // Check if the properties of the engineer object match the provided values
 
     expect(engineer.getName()).toBe(name);
     expect(engineer.getId()).toBe(id);
     expect(engineer.getEmail()).toBe(email);
     expect(engineer.getGithub()).toBe(github);
     expect(engineer.getRole()).toBe(role);
+
+    // Print out the engineer's information for debugging or verification purposes
 
     console.log(`Role: ${engineer.getRole()}, Engineer Name: ${engineer.getName()}, Engineer ID: ${engineer.getId()}, Engineer Email: ${engineer.getEmail()}, GitHub Username: ${engineer.getGithub()}`);
 });
