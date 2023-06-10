3c788e2db69adc340b2b7270551beab506e6f388
Jacob Milojkovich <jacobmilojkovich@jacobs-mbp.mynetworksettings.com>
2023-06-10 17:50:41 -0400

    added comments

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,14 @@
 const Employee = require('../lib/Employee');
 
+// Test case: checks if all information is provided and accurate
+
 test('checks if all info is provided and accurate', () => {
     const name = "John";
     const id = 1;
     const email = "John@bootcampemail.com";
     const role = "";
+
+    // Creating an instance of the Employee class with the provided information
 
     const employee = new Employee(name, id, email, role);
 
