43eb21390768a1ebc97f342f9b4990660341aa2d
Jacob Milojkovich <jacobmilojkovich@jacobs-mbp.mynetworksettings.com>
2023-06-10 17:57:07 -0400

    comments addded intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,12 +1,14 @@
 const Intern = require('../lib/Intern');
 
 test('checks if all info for Intern is provided and accurate', () => {
+    // Define test data
     const name = "John";
     const id = 1;
     const email = "John@bootcampemail.com";
     const school = "It's the best one ever";
     const role = "Intern";
 
+      // Create an instance of Intern
     const intern = new Intern(name, id, email, school);
 
     expect(intern.getName()).toBe(name);
@@ -14,6 +16,8 @@ test('checks if all info for Intern is provided and accurate', () => {
     expect(intern.getEmail()).toBe(email);
     expect(intern.getSchool()).toBe(school);
     expect(intern.getRole()).toBe(role);
+
+        // Print intern details for verification
 
     console.log(`Role: ${intern.getRole()}, Intern Name: ${intern.getName()}, Intern ID: ${intern.getId()}, Intern Email: ${intern.getEmail()}, School: ${intern.getSchool()}`);
 });
